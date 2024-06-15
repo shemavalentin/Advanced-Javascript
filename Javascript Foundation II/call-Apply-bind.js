d5132c0c5bd803e068b8e466c535dab4c5f270a4
Shema Valentin <shemavalentin@gmail.com>
2024-06-15 19:56:28 +0200

    Call(), Apply(), and Bind() methods use

---
+++ Javascript Foundation II/call-Apply-bind.js
@@ -0,0 +1,99 @@
+// these functions are mostly asked in seniol developers interviews
+
+function a() {
+  console.log("hi");
+}
+a.call(); // when we invoke a function evrytime there is that call method.
+// but we often use the shorthand and remove the call() and write a();
+a.apply(); // apply() also works the same as call(); they do the same thing
+
+/* Let's see more examples about call() and apply */
+// let's say a game
+
+const wizard = {
+  name: "Merlin",
+  health: 50,
+  heal() {
+    return (this.health = 100);
+  },
+};
+
+// let's create another object
+
+const archer = {
+  name: "Robbin Hood",
+  health: 30,
+
+  // Now what if we want to use the heal() method and we don't want to repeat ourselves by copying and pasting?
+  // what we need now is to be able to borrow using call and apply and remain DRY.
+  // We don't want to add to memory.
+};
+
+// how to do this??
+
+console.log("1", archer);
+wizard.heal.call(archer);
+console.log("2", archer);
+
+// ===========  call() can also receive parameters ============
+
+const wizard1 = {
+  name: "Merlin",
+  health: 50,
+  heal(num1, num2) {
+    return (this.health += num1 + num2);
+  },
+};
+
+// let's create another object
+
+const archer1 = {
+  name: "Robbin Hood",
+  health: 30,
+};
+
+// how to do this??
+console.log("1", archer1);
+wizard1.heal.call(archer1, 50, 30); // here call() keeps taking parameters, paramether
+wizard1.heal.apply(archer1, 50, 30); // here call() takes array of parameters. that's the only difference.
+console.log("2", archer1);
+
+// the choice of what to use will depend on if you need to pass a list of paramenter
+// or use arrays. the important thing is to see if the method you want to borrow has
+// the this keyword so that it can be dynamic
+
+/* What about BIND()?
+
+well, simillar to call() and apply(), bind allows us to use the method from another object
+wille the two immediately runs the function, bind() method returns a function
+with a certain parameters. it is usually used when we want a function to be called latter
+on with a certain type of context or certain type of this keyword.
+
+*/
+
+const wizard2 = {
+  name: "Merlin",
+  health: 50,
+  heal(num1, num2) {
+    return (this.health += num1 + num2);
+  },
+};
+
+// let's create another object
+
+const archer2 = {
+  name: "Robbin Hood",
+  health: 30,
+};
+
+// how to do this??
+console.log("1", archer1);
+
+// to use bind we have to declare it as a function to use latter on as described
+// the function to borrow should have the this keyword.
+// bind method allows us to store the method for latter use.
+const wizardArcher = wizard1.heal.bind(archer1, 100, 30);
+// then call the function to use it
+wizardArcher();
+
+console.log("2", archer1);
