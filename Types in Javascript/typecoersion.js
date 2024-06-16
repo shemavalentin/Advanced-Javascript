00c2001c85ae5907fa80452c3ef95d7d782bc110
Shema Valentin <shemavalentin@gmail.com>
2024-06-16 19:50:58 +0200

    about Type coersion

---
+++ Types in Javascript/typecoersion.js
@@ -0,0 +1,12 @@
+//JS can be weird when it comes to type coercion.
+//Try to guess what the output for each of the lines below are:
+
+false == "";
+false == [];
+false == {};
+"" == 0;
+"" == [];
+"" == {};
+0 == [];
+0 == {};
+0 == null;
