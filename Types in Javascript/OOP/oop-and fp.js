f52b03fb8166f2b4de0648ade473051d36d3d275
Shema Valentin <shemavalentin@gmail.com>
2024-06-25 09:08:09 +0200

    OOP and FP

---
+++ Types in Javascript/OOP/oop-and fp.js
@@ -0,0 +1,31 @@
+/*
+OOP : Object oriented Programming
+
+the OOP says that bringing together the data and it's behavior in a single location
+called object.  and containing all that in a box makes it easier to understand
+how our programs work.
+
+* in OOP is like building a robot: we have head componet, chest component, arms component
+legs component etc all built together.
+
+
+FUNCTIONAL PROGRAMMING: says that data and behavior are distinctly different things
+and should be kept differently for clarity.
+
+instead of having one giant box, we have multiple boxes.
+
+* FP says, just give a data, and I'll act upon that data through functions and I
+will return something new from the data that you gave me.
+*/
+
+// The beauty of javescript is that it is MULT-PARADIGMS: we can use both these
+// techniques to make our code easy to reason about, to make it clearer,
+// to make it more understandable, easy to extend, and more efficient.
+
+// we can use each of these paradigms based on our problems
+
+/* The 2 pillars of JS (Clausures and Prototypes). without clausure we are 
+not able to write functional programming. and without Prototypes, we could not be 
+able to  do OOP 
+
+*/
