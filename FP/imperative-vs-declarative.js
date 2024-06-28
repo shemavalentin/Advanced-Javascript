c8e995af61a9a09b0d0c2841e037844695a714f1
Shema Valentin <shemavalentin@gmail.com>
2024-06-28 17:46:51 +0200

    Declarative vs Imperative

---
+++ FP/imperative-vs-declarative.js
@@ -0,0 +1,35 @@
+// Imperative vs Declarative
+
+/*  Imperative code is  code that tells the machine what to do and how to do it.
+
+Declarative code is code that tell the machine what to do and what should happen.
+It doen't tell the computer how to do things.
+
+Let's think it this way.  => a computer is better at being imperative. means it 
+needs to know how to do things.
+
+We as humans are more declarative. if I tell my friend, hey can you pass me that
+jug of water? I don't need to tell him, wolk over ther, ....... and give me that
+jug
+
+*/
+
+// example of imperative(for loops are pretty more of imperative)
+for (i = 0; i < 1000; i++) {
+  // this is imperative due to many instructions geiven here.
+  console.log(i);
+}
+
+// but we can make declarative
+[]
+  .forEach((item) => console.log(item))[123]
+  .forEach((item) => console.log(item)); // this declarative: we don't tell the
+// program what to do, or how to do it.
+
+// The other example of imperative is jQuery
+
+// Other front end frameworks like React are Declarative.
+
+// Why are learning this? well, because FP is more declarative.
+
+// In Function Composition we tell the function what to do not how to do it.
