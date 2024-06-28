f8071b4ae492d0a0a1ddae3c209ca2f1d4ac764b
Shema Valentin <shemavalentin@gmail.com>
2024-06-28 09:12:05 +0200

    Introduction to functional programming

---
+++ FP/fp-introduction.js
@@ -0,0 +1,32 @@
+/*
+WHAT IS FUNCTIONAL PROGRAMMING?
+
+Functional programming is all about separation of conserns,
+which object oriented programming does as well.
+
+It is all about packaging our code into separate chunks
+so that everything is well organized, and each part of our code
+concerns itself with one thing it's good at.
+
+In functional Programming there is this idea of separation of concern,
+but they also separate data and function.
+
+That is a function programmer views the world as, well, there is data
+and then this data gets interacted with. there is no real function but 
+functional programming puts imphasis on simplicity where data and 
+functions are concerned. we separate them up.
+
+
+When it comes to functional programming, we have only one pillar:
+PURE FUNCTIONS. and unlike in OOP where we have 4 pillars such as
+ENCAPSULATION, ABSTRACTION, POLYMORPHISM, AND INHERTANCE  in FP we
+only have one. if you want to break things down in FP, you use this
+answer. Pure functions.
+
+The idea in Pure Function is the separation of data of a program,
+and the behavior of a program. and all objects created in functional
+programming are immutable(Once something is created, it can not change),
+we avoid things like shared state, and we adhere to the principal of
+pure function.
+
+*/
