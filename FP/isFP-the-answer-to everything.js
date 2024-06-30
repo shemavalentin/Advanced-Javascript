2d1b9c801a556bd219b4768e5bfa684b3d077586
Shema Valentin <shemavalentin@gmail.com>
2024-06-30 19:29:14 +0200

    Answer to the question about FP

---
+++ FP/isFP-the-answer-to everything.js
@@ -0,0 +1,34 @@
+/*
+WHY ARE PEOPLE ARE SO EXCITED ABOUT FUNCTINAL PROGRAMMING?
+
+The idea with functional programming is this idea of separation 
+of data and functions or data and the effects that happen on that data.
+
+If you try to perform effects and logic at the same time, the problem is that
+you may create hidden side effects which cause a lot of bugs.
+
+When you use the fp principles as follow, you build something complex but with
+small parts that build big things.
+
+1. Task
+2. Returns statement
+3. pure
+4. No share state
+5. Immutable state
+6. Composable
+7. Predictable
+
+
+
+It doesn't mean that functional programming the answer to everything. 
+Yes because of it's nature, it works very well with distributed systems
+that have little different machine all over the world working with each orher
+
+or parallelism where multiple things have to happen in  parallel, but because
+there functions are pure, they don't cause strange bugs
+
+
+BUT IT DEPENDS ON THE PROBLEM YOU HAVE.
+
+There a time that OOP could be a good answer to the priblem.
+*/
